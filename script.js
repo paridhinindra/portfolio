c29405f5a8346201f7721c920996e84a6a697713
paridhinindra <97993279+paridhinindra@users.noreply.github.com>
2022-02-07 16:58:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Artist", "Future Business Analyst", "Designer"],
+        strings: ["Student", "Upcoming Business Analyst", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Artist", "Future Business Analyst", "Designer"],
+        strings: ["Student", "Upcoming Business Analyst", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
